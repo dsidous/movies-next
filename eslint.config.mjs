0666f19e7fde26a062b0bcbccd62ab390fe4097d
dsidous <jonastamas2@gmail.com>
2026-04-24 15:40:50 +0700

    Add Prettier configuration, update dependencies, and enhance TypeScript paths

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +14,20 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  prettier, // Must be last to override other configs
 ]);
 
 export default eslintConfig;
+
+
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const compat = new FlatCompat({
+//   baseDirectory: import.meta.dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+
+// export default eslintConfig;
