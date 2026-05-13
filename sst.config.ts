e0f03f141960f47144f0d69be3129f5716c335e6
dsidous <jonastamas2@gmail.com>
2026-05-13 15:07:26 +0700

    Refactor SST configuration to standardize string formatting and add missing environment variables for improved application setup.

--- sst.config.ts
+++ sst.config.ts
@@ -4,23 +4,29 @@
 export default $config({
   app(input) {
     return {
-      name: "movies-next",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      home: "aws",
+      name: 'movies-next',
+      removal: input?.stage === 'production' ? 'retain' : 'remove',
+      home: 'aws',
       providers: {
-        cloudflare: "6.15.0",
+        cloudflare: '6.15.0',
       },
     };
   },
   async run() {
-    new sst.aws.Nextjs("MyMovieApp", {
+    new sst.aws.Nextjs('MyMovieApp', {
       domain: {
-        name: "watch.tamasjonas.com",
+        name: 'watch.tamasjonas.com',
         dns: sst.cloudflare.dns(), // This tells SST to use Cloudflare for DNS
       },
       environment: {
-        NEXT_PUBLIC_TMDB_API_KEY: process.env.NEXT_PUBLIC_TMDB_API_KEY!,
         CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY!,
+        CLERK_WEBHOOK_SECRET: process.env.CLERK_WEBHOOK_SECRET!,
+        DATABASE_URL: process.env.DATABASE_URL!,
+        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!,
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        TMDB_API_TOKEN: process.env.TMDB_API_TOKEN!,
+        TMDB_BASE_URL: process.env.TMDB_BASE_URL!,
       },
     });
   },
