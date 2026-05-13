a7bbb3564b7fa91d8dcf15b4c73a61fb6b3e5bc6
dsidous <jonastamas2@gmail.com>
2026-05-13 12:58:37 +0700

    Update SST configuration to use the latest Cloudflare provider version and standardize string formatting. Add .sst/ to .gitignore to prevent unnecessary files from being tracked.

--- sst.config.ts
+++ sst.config.ts
@@ -1,16 +1,18 @@
 export default $config({
   app(input) {
     return {
-      name: 'movies-next',
-      removal: input?.stage === 'production' ? 'retain' : 'remove',
-      home: 'aws',
-      providers: { cloudflare: '0.1.0' }, // Add the provider here
+      name: "movies-next",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+      providers: {
+        cloudflare: "6.15.0",
+      },
     };
   },
   async run() {
-    new sst.aws.Nextjs('MyMovieApp', {
+    new sst.aws.Nextjs("MyMovieApp", {
       domain: {
-        name: 'watch.tamasjonas.com',
+        name: "watch.tamasjonas.com",
         dns: sst.cloudflare.dns(), // This tells SST to use Cloudflare for DNS
       },
       environment: {
